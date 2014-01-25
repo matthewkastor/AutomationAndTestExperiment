9e4c9e38aeabf500840df114562872665ceb3a25
Matthew Kastor <matthewkastor@gmail.com>
2014-01-25 18:03:45 -0500

    adds portable class library

---
+++ AutomationAndTestExperiment_PortableClassLib/MyStaticClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomationAndTestExperiment_PortableClassLib
+{
+    public class MyStaticClass
+    {
+        public static string SayHi()
+        {
+            //return String.Empty;
+            return "Hi";
+        }
+    }
+}
