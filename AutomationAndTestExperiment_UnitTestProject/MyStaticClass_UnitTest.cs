4ea84a265afd1420c72e3be8c062d78ec831e849
Matthew Kastor <matthewkastor@gmail.com>
2014-01-25 18:03:06 -0500

    adds unit test project

---
+++ AutomationAndTestExperiment_UnitTestProject/MyStaticClass_UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutomationAndTestExperiment_UnitTestProject
+{
+    [TestClass]
+    public class MyStaticClass_UnitTest
+    {
+        [TestMethod]
+        public void SayHi_TestMethod()
+        {
+            string result = AutomationAndTestExperiment_PortableClassLib
+                .MyStaticClass.SayHi();
+            Assert.AreEqual("Hi", result);
+        }
+    }
+}
